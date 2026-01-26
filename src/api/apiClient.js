69fbd15eba3ed33f7e90d8a3f3e566ec27ce6031
Bidipta Gogoi <gogoibidipta@gmail.com>
2026-01-26 23:53:25 +0530

    complete

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const apiClient = axios.create({
     // USE YOUR IP, NOT localhost
-    // baseURL: 'http://43.205.113.58/api/greeting-app', //prod
-    baseURL: 'http://192.168.31.88:3000/api/greeting-app', //local
+    baseURL: 'http://43.205.113.58/api/greeting-app', //prod
+    // baseURL: 'http://192.168.31.88:3000/api/greeting-app', //local
     timeout: 10000,
 });
 
